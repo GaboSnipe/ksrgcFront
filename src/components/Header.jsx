d2a90d202400a84eddb2fc4d55c7b9f81b4becb8
GaboSnipe <gabriel.gulua@geolab.edu.ge>
2024-06-19 02:56:30 +0400

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,7 @@ const Header = () => {
   const loginState = useSelector((state) => state.auth.isLoggedIn);
   const [isOpen, setIsOpen] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFiles, setSelectedFiles] = useState([]);
   const [documentNumber, setDocumentNumber] = useState("");
   const [title, setTitle] = useState("");
   const [coment, setComent] = useState("");
@@ -32,6 +33,14 @@ const Header = () => {
   const handleDocUpload = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
+  };
+  const handleDocsUpload = (event) => {
+    const files = Array.from(event.target.files); // Convert FileList to Array
+    setSelectedFiles([...selectedFiles, ...files]); // Concatenate new files with existing files
+  };
+  const handleRemoveFile = (fileToRemove) => {
+    const filteredFiles = selectedFiles.filter(file => file !== fileToRemove);
+    setSelectedFiles(filteredFiles);
   };
 
   const handleDocumentNumberChange = (e) => {
@@ -110,136 +119,174 @@ const Header = () => {
 
       {isOpen && (
         <>
-          <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="popup-backdrop" onClick={togglePopup}></div>
-            <div className="popup w-full md:w-3/4 rounded-lg shadow-lg p-4">
-              <h1 className="text-xl text-center text-accent-content mb-4">ახალი დოკუმენტის მომზადება</h1>
-              <div className="divide-y divide-slate-700">
-                <div className="flex items-center justify-center gap-2 py-2">
-                  <label className="block w-1/2 text-xl font-medium text-white-700">
-                    documentNumber:
-                  </label>
-                  <input
-                    id="documentNumber"
-                    type="number"
-                    value={documentNumber}
-                    onChange={handleDocumentNumberChange}
-                    className="border rounded-lg px-3 py-2 mt-1 text-sm w-full"
-                  />
-                </div>
+  <div className="fixed inset-0 flex items-center justify-center z-50">
+    <div className="popup-backdrop" onClick={togglePopup}></div>
+    <div className="popup w-full md:w-3/4 rounded-lg shadow-lg p-4">
+      <h1 className="text-xl text-center text-accent-content mb-4">ახალი დოკუმენტის მომზადება</h1>
 
-                <div className="flex items-center justify-center gap-2 py-2">
-                  <label className="block w-1/2 text-xl font-medium text-white-700">
-                    title:
-                  </label>
-                  <input
-                    id="title"
-                    type="text"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    className="border rounded-lg px-3 py-2 mt-1 text-sm w-full"
-                  />
-                </div>
+      <div className="divide-y divide-slate-700">
+        <div className="grid grid-cols-5 gap-4 py-2 items-center">
+          <label className="text-xl font-medium text-white-700 col-span-1">documentNumber:</label>
+          <input
+            id="documentNumber"
+            type="number"
+            value={documentNumber}
+            onChange={handleDocumentNumberChange}
+            className="border rounded-lg px-3 py-2 mt-1 text-sm col-span-4"
+          />
+        </div>
 
-                <div className="flex items-center justify-center gap-2 py-2">
-                  <label className="block w-1/2 text-xl font-medium text-white-700">
-                    დოკუმენტი:
-                  </label>
-                  <input
-                    id="docUpload"
-                    type="file"
-                    onChange={handleDocUpload}
-                    className="hidden"
-                  />
-                  <label htmlFor="docUpload" className="block w-full font-medium text-white-700 border rounded-lg px-3 py-2 mt-1 text-sm cursor-pointer bg-gray-800 hover:bg-gray-700 text-white">
-                    {selectedFile ? `${selectedFile.name}` : 'ფაილის ატვირთვა'}
-                  </label>
-                </div>
+        <div className="grid grid-cols-5 gap-4 py-2 items-center">
+          <label className="text-xl font-medium text-white-700 col-span-1">title:</label>
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border rounded-lg px-3 py-2 mt-1 text-sm col-span-4"
+          />
+        </div>
 
-                <div className="flex items-center justify-center gap-2 py-2">
-                  <label className="block w-1/2 text-xl font-medium text-white-700">
-                    coment:
-                  </label>
-                  <textarea
-                    id="coment"
-                    value={coment}
-                    onChange={(e) => setComent(e.target.value)}
-                    className="border rounded-lg px-3 py-2 mt-1 text-sm w-full"
-                    style={{ maxHeight: "40vh", minHeight: "15vh", overflowY: "auto" }}
-                  />
-                </div>
-                <div className="flex items-center justify-center gap-2 py-2">
-                  <label className="block w-1/2 text-xl font-medium text-white-700">
-                    haveToSign:
-                  </label>
-                  <input
-                    id="haveToSign"
-                    type="text"
-                    value={haveToSign}
-                    onChange={(e) => setHaveToSign(e.target.value)}
-                    className="border rounded-lg px-3 py-2 mt-1 text-sm w-full"
-                  />
-                </div>
+        <div className="grid grid-cols-5 gap-4 py-2 items-center">
+          <label className="text-xl font-medium text-white-700 col-span-1">დოკუმენტი:</label>
+          <div className="relative col-span-4">
+            <input
+              id="docUpload"
+              type="file"
+              onChange={handleDocUpload}
+              className="hidden"
+            />
+            <label
+              htmlFor="docUpload"
+              className="block font-medium text-white-700 border rounded-lg px-3 py-2 mt-1 text-sm cursor-pointer bg-gray-800 hover:bg-gray-700 text-white text-center"
+            >
+              {selectedFile ? `${selectedFile.name}` : 'ფაილის ატვირთვა'}
+            </label>
+          </div>
+        </div>
 
-                <div className="flex items-center gap-2 py-2">
-                  <label className="block text-xl font-medium text-white-700" htmlFor="haveToSign">
-                    haveToVerify:
-                  </label>
-                  <div className="relative flex-grow">
-                    <div className="flex flex-wrap border rounded-lg px-3 py-2 mt-1 text-sm w-full">
-                      {haveToVerify.map((email, index) => (
-                        <div key={index} className="flex items-center rounded">
-                          <span>{email}</span>
-                          <button
-                            type="button"
-                            onClick={() => handleRemoveHaveToVerify(index)}
-                            className="ml-1 text-red-600"
-                          >
-                            &times;
-                          </button>
-                        </div>
-                      ))}
-                      <input
-                        id="haveToVerify"
-                        type="text"
-                        value={haveToVerifySearch}
-                        onChange={(e) => setHaveToVerifySearch(e.target.value)}
-                        onFocus={() => setIsFocused(true)}
-                        onBlur={() => setTimeout(() => setIsFocused(false), 200)}
-                        className="border-none flex-grow ml-2 focus:outline-none"
-                      />
-                      {isFocused && haveToVerifySearch && (
-                        <div className="absolute z-10 border rounded-lg shadow-lg mt-1 w-full">
-                          {filteredOptions.map((option) => (
-                            <div
-                              key={option.id}
-                              onClick={() => handleAddHaveToVerify(option.email)}
-                              className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-                            >
-                              {option.email}
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </div>
-
-
-
-                <div className="flex justify-center py-2">
+        <div className="grid grid-cols-5 gap-4 py-2 items-center">
+      <label className="text-xl font-medium text-white-700 col-span-1">დოკუმენტები:</label>
+      <div className="relative col-span-4">
+        <input
+          id="docsUpload"
+          type="file"
+          onChange={handleDocsUpload}
+          className="hidden"
+          multiple  // Allow multiple files to be selected
+        />
+        <label
+          htmlFor="docsUpload"
+          className="block font-medium text-white border rounded-lg px-3 py-2 mt-1 text-sm cursor-pointer bg-gray-800 hover:bg-gray-700 text-white text-center w-full"
+        >
+          {selectedFiles.length === 0 ? (
+            'ფაილის ატვირთვა'
+          ) : (
+            <div className="flex flex-wrap gap-2">
+              {selectedFiles.map((file, index) => (
+                <div
+                  key={index}
+                  className="bg-gray-800 rounded-lg p-2 flex items-center cursor-pointer"
+                  onClick={() => handleFileClick}
+                >
+                  <span className="mr-2">{file.name}</span>
                   <button
                     type="button"
-                    onClick={uploadDoc}
-                    className="btn bg-blue-600 hover:bg-green-600 text-white btn-sm mx-2"
+                    onClick={() => handleRemoveFile(file)}
+                    className="text-red-600 hover:text-red-700 focus:outline-none text-lg" // Increased text size for the delete button
                   >
-                    დოკუმენტის შექმნა
+                    X
                   </button>
                 </div>
-              </div>
+              ))}
+            </div>
+          )}
+        </label>
+      </div>
+    </div>
+        <div className="grid grid-cols-5 gap-4 py-2 items-center">
+          <label className="text-xl font-medium text-white-700 col-span-1">coment:</label>
+          <textarea
+            id="coment"
+            value={coment}
+            onChange={(e) => setComent(e.target.value)}
+            className="border rounded-lg px-3 py-2 mt-1 text-sm col-span-4"
+            style={{ maxHeight: '40vh', minHeight: '15vh', overflowY: 'auto' }}
+          />
+        </div>
+
+        <div className="grid grid-cols-5 gap-4 py-2 items-center">
+          <label className="text-xl font-medium text-white-700 col-span-1">haveToSign:</label>
+          <input
+            id="haveToSign"
+            type="text"
+            value={haveToSign}
+            onChange={(e) => setHaveToSign(e.target.value)}
+            className="border rounded-lg px-3 py-2 mt-1 text-sm col-span-4"
+          />
+        </div>
+
+        <div className="grid grid-cols-5 gap-4 py-2 items-center">
+          <label className="text-xl font-medium text-white-700 col-span-1">haveToVerify:</label>
+          <div className="relative col-span-4">
+            <div className="flex flex-wrap border rounded-lg px-3 py-2 mt-1 text-sm w-full p-4" style={{backgroundColor: "#121212"}}>
+              {haveToVerify.map((email, index) => (
+                <div key={index} className="relative flex items-center rounded text-white hover:bg-gray-800  text-xs">
+                  <span className="flex ml-3 mr-1  text-xs">{email}</span>
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveHaveToVerify(index)}
+                    className="flex right-0 top-0 transform -translate-y-1 text-red-600 ml-1 mr-2  text-xs"
+                  >
+                    &times;
+                  </button>
+                </div>
+              ))}
+              <input
+                id="haveToVerify"
+                type="text"
+                value={haveToVerifySearch}
+                onChange={(e) => setHaveToVerifySearch(e.target.value)}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setTimeout(() => setIsFocused(false), 200)}
+                className="border-none flex-grow ml-2 focus:outline-none text-white"
+              />
+              {isFocused && haveToVerifySearch && (
+                <div className="absolute left-0 z-10 border rounded-lg shadow-lg mt-1 w-full bg-gray-900 transform translate-y-6">
+                  {filteredOptions.map((option) => (
+                    <div
+                      key={option.id}
+                      onClick={() => handleAddHaveToVerify(option.email)}
+                      className="px-4 py-2 cursor-pointer hover:bg-gray-800 rounded-lg"
+                    >
+                      {option.email}
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
-        </>
+        </div>
+
+        <div className="flex justify-center py-2">
+          <button
+            type="button"
+            onClick={uploadDoc}
+            className="btn bg-blue-600 hover:bg-green-600 text-white btn-sm mx-2"
+          >
+            დოკუმენტის შექმნა
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+
+
+
+
+
+
       )}
     </>
   );
